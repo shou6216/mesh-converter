c8e6252c0ca9fc61c87f8ceb64f086dd960cdea8
shou6216 <shou-y6216@yoshinoda.com>
2017-05-13 22:47:24 +0900

    StopWatch追加

--- src/main/java/com/yoshinoda/shou6216/RegisterRun.java
+++ src/main/java/com/yoshinoda/shou6216/RegisterRun.java
@@ -3,6 +3,7 @@
  */
 package com.yoshinoda.shou6216;
 
+import org.apache.commons.lang3.time.StopWatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,13 +19,23 @@ public class RegisterRun {
 	private static final Logger LOGGER = LoggerFactory.getLogger(RegisterRun.class);
 
 	public static void main(String[] args) {
+		StopWatch sw = new StopWatch();
+		sw.start();
+		
+		LOGGER.info("RegisterRun start");
+		
 		RegisterRun registerRun = new RegisterRun();
 		registerRun.execute();
+		
+		sw.stop();
+		LOGGER.info("RegisterRun end {}[msec]", sw.getTime());
 	}
 	
 	public void execute() {
-		LOGGER.info("execute start");
-		LOGGER.info("execute end");
+
+		
+		
+
 	}
 
 }
