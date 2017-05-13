5b1557c0a253829fa1213490fbe0f7e0759532c1
shou6216 <shou-y6216@yoshinoda.com>
2017-05-13 23:42:43 +0900

    toListを表示するようにしました。

--- src/main/java/com/yoshinoda/shou6216/RegisterRun.java
+++ src/main/java/com/yoshinoda/shou6216/RegisterRun.java
@@ -9,7 +9,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -17,6 +19,8 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.StopWatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.yoshinoda.shou6216.model.AreaMeshRoute;
 
 
 
@@ -30,6 +34,8 @@ public class RegisterRun {
 	private static final Logger LOGGER = LoggerFactory.getLogger(RegisterRun.class);
 	
 	private static final String FROM_FILE = "fromList.txt";
+	private static final String TO_FILE = "toList.txt";
+	private static final int TO_COLUMN = 4;
 
 	public static void main(String[] args) {
 		StopWatch sw = new StopWatch();
@@ -50,19 +56,21 @@ public class RegisterRun {
 			LOGGER.info("fromMeshCodeList is empty");
 			return;
 		}
-		
+
 		LOGGER.info("Number of fromMeshCodeList : {}", fromMeshCodeList.size());
 		for (int fromMeshCode : fromMeshCodeList) {
 			LOGGER.info("fromMeshCode : {}", fromMeshCode);
+			
+			Set<AreaMeshRoute> toMeshSet = getToMeshList(fromMeshCode);
+			for (AreaMeshRoute toMesh : toMeshSet) {
+				LOGGER.info("toMeshCode : {}", toMesh);
+			}
 		}
-		
-		
-
 	}
 
 	private List<Integer> getFromMeshCodeList() {
 		Path path = Paths.get(FROM_FILE);
-		LOGGER.info("fromAreaMeshList path={}", path);
+		LOGGER.info("fromMeshCodeList path={}", path);
 		
 		List<Integer> fromMeshCodeList = new ArrayList<Integer>();
 		try (Stream<String> stream = Files.lines(path, Charset.forName("UTF-8"))) {
@@ -75,5 +83,29 @@ public class RegisterRun {
 			LOGGER.error("read fromMeshCodeList error", e);
 		}
 		return fromMeshCodeList;
+	}
+	
+	private Set<AreaMeshRoute> getToMeshList(int fromMeshCode) {
+		Path path = Paths.get(TO_FILE);
+		LOGGER.info("toMeshList path={}", path);
+		
+		Set<AreaMeshRoute> toMeshSet = new HashSet<AreaMeshRoute>();
+		try (Stream<String> stream = Files.lines(path, Charset.forName("UTF-8"))) {
+			toMeshSet = stream
+				.map(line -> line.split(","))
+				.filter(values -> values.length == TO_COLUMN)
+				.map(values -> new AreaMeshRoute(
+						fromMeshCode,
+						Integer.valueOf(values[0]),
+						Integer.valueOf(values[1]),
+						Integer.valueOf(values[2]),
+						Integer.valueOf(values[3])
+						))
+				.collect(Collectors.toSet());
+			
+		} catch (IOException e) {
+			LOGGER.error("read toMeshList error", e);
+		}
+		return toMeshSet;
 	}
 }
--- src/main/java/com/yoshinoda/shou6216/model/AreaMeshRoute.java
+++ src/main/java/com/yoshinoda/shou6216/model/AreaMeshRoute.java
@@ -3,9 +3,52 @@
  */
 package com.yoshinoda.shou6216.model;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 /**
  * @author shou_y
  *
  */
 public class AreaMeshRoute {
+	
+	private int fromMeshCode;
+	private int toMeshCode;
+	private int timeWalk;
+	private int timeCar;
+	private int timeTrain;
+	
+	public AreaMeshRoute(int fromMeshCode, int toMeshCode,
+			int timeWalk, int timeCar, int timeTrain) {
+		this.fromMeshCode = fromMeshCode;
+		this.toMeshCode = toMeshCode;
+		this.timeWalk = timeWalk;
+		this.timeCar = timeCar;
+		this.timeTrain = timeTrain;
+	}
+
+	public int getFromMeshCode() {
+		return fromMeshCode;
+	}
+
+	public int getToMeshCode() {
+		return toMeshCode;
+	}
+
+	public int getTimeWalk() {
+		return timeWalk;
+	}
+
+	public int getTimeCar() {
+		return timeCar;
+	}
+
+	public int getTimeTrain() {
+		return timeTrain;
+	}
+	
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this ,ToStringStyle.SHORT_PREFIX_STYLE);
+	}
 }
