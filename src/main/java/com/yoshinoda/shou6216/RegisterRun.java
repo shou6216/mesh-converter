aaf5986eb4d19f119ca75ba352468a7e5c5c2c8b
shou6216 <shou-y6216@yoshinoda.com>
2017-05-13 23:24:42 +0900

    fromファイルを読み込めるようにしました

--- src/main/java/com/yoshinoda/shou6216/RegisterRun.java
+++ src/main/java/com/yoshinoda/shou6216/RegisterRun.java
@@ -3,6 +3,17 @@
  */
 package com.yoshinoda.shou6216;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.StopWatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +28,8 @@ import org.slf4j.LoggerFactory;
 public class RegisterRun {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(RegisterRun.class);
+	
+	private static final String FROM_FILE = "fromList.txt";
 
 	public static void main(String[] args) {
 		StopWatch sw = new StopWatch();
@@ -32,10 +45,35 @@ public class RegisterRun {
 	}
 	
 	public void execute() {
-
+		List<Integer> fromMeshCodeList = getFromMeshCodeList();
+		if (fromMeshCodeList.isEmpty()) {
+			LOGGER.info("fromMeshCodeList is empty");
+			return;
+		}
+		
+		LOGGER.info("Number of fromMeshCodeList : {}", fromMeshCodeList.size());
+		for (int fromMeshCode : fromMeshCodeList) {
+			LOGGER.info("fromMeshCode : {}", fromMeshCode);
+		}
 		
 		
 
 	}
 
+	private List<Integer> getFromMeshCodeList() {
+		Path path = Paths.get(FROM_FILE);
+		LOGGER.info("fromAreaMeshList path={}", path);
+		
+		List<Integer> fromMeshCodeList = new ArrayList<Integer>();
+		try (Stream<String> stream = Files.lines(path, Charset.forName("UTF-8"))) {
+			fromMeshCodeList = stream
+				.filter(line -> StringUtils.isNotBlank(line))
+				.map(line -> Integer.valueOf(line))
+				.collect(Collectors.toList());
+			
+		} catch (IOException e) {
+			LOGGER.error("read fromMeshCodeList error", e);
+		}
+		return fromMeshCodeList;
+	}
 }
---
+++ src/main/java/com/yoshinoda/shou6216/model/AreaMeshRoute.java
@@ -0,0 +1,11 @@
+/**
+ * yoshinoda.com
+ */
+package com.yoshinoda.shou6216.model;
+
+/**
+ * @author shou_y
+ *
+ */
+public class AreaMeshRoute {
+}
